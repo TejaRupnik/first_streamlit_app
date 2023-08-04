7319053cc1627fb24cbe8880579cb37dda63c167
TejaRupnik <141241950+TejaRupnik@users.noreply.github.com>
2023-08-04 09:15:35 +0200

    Update streamlit_app.py
    !

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner!')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Othmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
